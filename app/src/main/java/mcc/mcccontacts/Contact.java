5e6b6c90c68504a86c473eb66f2ef8d61fbb456d
pereidc1 <cesar.pereida@aalto.fi>
2014-11-30 18:49:02 +0200

    .iml added

--- app/src/main/java/mcc/mcccontacts/Contact.java
+++ app/src/main/java/mcc/mcccontacts/Contact.java
@@ -1,40 +1,88 @@
 package mcc.mcccontacts;
 
+import java.io.Serializable;
+
 /**
  * Created by eduardocastellanosn on 11/29/14.
  */
-public class Contact {
-    public Contact(){
+public class Contact implements Serializable {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String mobile;
+    private String address;
+    private String id;
+
+    public Contact(String firstName, String lastName, String email, String phoneNumber, String mobile, String address, String id){
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.mobile = mobile;
+        this.address = address;
+        this.id = id;
     }
 
     public String getName(){
+        return firstName;
+    }
 
-        return "TestName";
+    public String getLastName(){
+        return lastName;
     }
 
     public String getFullName(){
         return this.getName() + " " + this.getLastName();
     }
 
-    public String getLastName(){
-        return "TestLastName";
-    }
-
     public String getPhoneNumber(){
-        return "+52 474-332-323";
+        return phoneNumber;
     }
 
     public String getMobileNumber(){
-        return "+358 23-323-444";
+        return mobile;
     }
 
     public String getAddress(){
-        return "Jamerantaival 5, Espoo";
+        return address;
     }
 
     public String getEmail(){
-        return "test@testemail.com";
+        return email;
     }
 
+    public String getId(){
+        return id;
+    }
+
+    public void setName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setMobileNumber(String mobile){
+        this.mobile = mobile;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
 }
--- app/src/main/java/mcc/mcccontacts/MainActivity.java
+++ app/src/main/java/mcc/mcccontacts/MainActivity.java
@@ -1,19 +1,62 @@
 package mcc.mcccontacts;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private SimpleAdapter adpt;
+    List<Contact> contactList = new ArrayList<Contact>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        adpt  = new SimpleAdapter(contactList, this);
+        ListView lView = (ListView) findViewById(R.id.listview);
+
+        lView.setAdapter(adpt);
+
+        (new RetrieveContacts()).execute("http://cloudguest116.niksula.hut.fi:8080/contacts/");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +77,83 @@ public class MainActivity extends Activity {
             return true;
         }
 
+        if (id == R.id.action_refresh) {
+            (new RetrieveContacts()).execute("http://cloudguest116.niksula.hut.fi:8080/contacts/");
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    public class RetrieveContacts extends AsyncTask<String, Void, List<Contact>>{
+
+        private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPostExecute(List<Contact> result) {
+            super.onPostExecute(result);
+            dialog.dismiss();
+            adpt.setItemList(result);
+            adpt.notifyDataSetChanged();
+            contactList = result;
+            Log.e("Contactos", contactList.toString());
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Downloading contacts...");
+            dialog.show();
+        }
+
+        protected List<Contact> doInBackground(String... params) {
+
+            List<Contact> result = new ArrayList<Contact>();
+
+            try {
+                URL u = new URL(params[0]);
+
+                HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+                conn.setRequestMethod("GET");
+
+                conn.connect();
+                InputStream is = conn.getInputStream();
+
+                // Read the stream
+                byte[] b = new byte[1024];
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                while ( is.read(b) != -1)
+                    baos.write(b);
+
+                String JSONResp = new String(baos.toByteArray());
+                JSONObject JObject = new JSONObject(JSONResp);
+
+                JSONArray JArr = JObject.getJSONArray("contacts");
+
+                for (int i=0; i < JArr.length(); i++) {
+                    result.add(convertContact(JArr.getJSONObject(i)));
+                }
+
+                return result;
+            }
+            catch(Throwable t) {
+                t.printStackTrace();
+            }
+            return null;
+        }
+
+        private Contact convertContact(JSONObject obj) throws JSONException {
+            String firstName = obj.getString("first_name");
+            String lastName = obj.getString("last_name");
+            String email = obj.getString("email");
+            String phoneNumber = obj.getString("phone");
+            String mobile = obj.getString("mobile");
+            String address = obj.getString("address");
+            String id = obj.getString("_id");
+
+            return new Contact(firstName, lastName, email, phoneNumber, mobile, address, id);
+        }
+    }
+
+
 }
